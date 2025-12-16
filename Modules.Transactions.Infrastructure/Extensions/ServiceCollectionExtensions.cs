4d27d38cbd81e2eb85d21a1ede89219abccd07f9
MajdArous <somethingsomeone036+12@gmail.com>
2025-12-16 23:33:14 +0300

    ConnectionString

--- Modules.Transactions.Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Modules.Transactions.Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -13,10 +13,10 @@ public static class ServiceCollectionExtensions
     public static void AddTransactionsInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         //"Server=(localdb)\\mssqllocaldb;Database=BankingSystemDb;Trusted_Connection=True;"
-        //  var connectionString = configuration.GetConnectionString("TemplateDb");
+        var connectionString = configuration.GetConnectionString("Database");
         //Server=db34639.public.databaseasp.net; Database=db34639; User Id=db34639; Password=3Zk@S_4o=yB8; Encrypt=True; TrustServerCertificate=True; 
         //services.AddDbContext<TransactionsDbContext>(options => options.UseSqlServer("Server=db34639.public.databaseasp.net; Database=db34639; User Id=db34639; Password=3Zk@S_4o=yB8; Encrypt=True; TrustServerCertificate=True; "));
-        services.AddDbContext<TransactionsDbContext>(options => options.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=BankingSystemDb;Trusted_Connection=True;"));
+        services.AddDbContext<TransactionsDbContext>(options => options.UseSqlServer(connectionString));
         services.AddScoped<ITransactionRulesSeeder, TransactionRulesSeeder>();
         services.AddScoped<ITransactionRulesRepository, TransactionRulesRepository>();
         services.AddScoped<ITransactionsRepository, TransactionsRepository>();
